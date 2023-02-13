fc43a877d55af868edbedce5b76019cce9d9de4c
Stephen McGuckin <stephen.mcguckin@nordicsemi.no>
2023-02-12 19:34:36 -0800

    updates the domain name and record name

--- infrastructure/create-stack.ts
+++ infrastructure/create-stack.ts
@@ -6,6 +6,6 @@ import { SiteStack } from './app-stack';
 const app = new cdk.App();
 new SiteStack(app, 'SiteStack', {
   env: { account: process.env['CDK_DEFAULT_ACCOUNT'], region: process.env['CDK_DEFAULT_REGION']},
-  recordName: 'teddy.mcguckins.me',
-  domainName: 'mcguckins.me'
+  recordName: 'teddy.pdxdoodle.info',
+  domainName: 'pdxdoodle.info'
 });
